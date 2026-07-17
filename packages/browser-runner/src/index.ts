bff4a02cf0352bf3aba34f2db0c54dc560666c7f
Prateek Gautam <59331424+the-illuzion@users.noreply.github.com>
2026-07-17 10:08:27 +0530

    Export browser report sanitizers

--- packages/browser-runner/src/index.ts
+++ packages/browser-runner/src/index.ts
@@ -1,6 +1,14 @@
 export { runBrowserJourney } from "./runner.js";
 export { installNetworkFixtures, installSiteRoute, matchesRequest } from "./network.js";
-export { fingerprintUrl, locatorFor, sanitizeArtifactName, shouldCapture } from "./util.js";
+export {
+  fingerprintText,
+  fingerprintUrl,
+  locatorFor,
+  sanitizeArtifactName,
+  sanitizePageUrl,
+  selectFailedRequests,
+  shouldCapture,
+} from "./util.js";
 export type {
   BrowserActionResult,
   BrowserArtifactManifest,
